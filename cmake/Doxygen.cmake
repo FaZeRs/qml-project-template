3fa43bb66343bcd8953cb8a72a02bb7b9f1e9cd1
Nauris Linde <naurislinde@gmail.com>
2023-05-17 13:51:10 +0000

    WIP

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -1,26 +1,3 @@
-#set(BUILD_DOCS FALSE CACHE BOOL "Build documentation using Doxygen")
-#if (BUILD_DOCS)
-#    find_package(Doxygen)
-#    # check if Doxygen is installed
-#    if(NOT DOXYGEN_FOUND)
-#        message(FATAL_ERROR "Doxygen is needed to build the documentation.")
-#    endif()
-#    # set input and output files
-#    set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile.in)
-#    set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
-#
-#    # request to configure the file
-#    configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
-#    message("Doxygen build started")
-#
-#    # note the option ALL which allows to build the docs together with the application
-#    add_custom_target(docs ALL
-#            COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
-#            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-#            COMMENT "Generating API documentation with Doxygen"
-#            VERBATIM )
-#endif()
-
 # Enable doxygen doc builds of source
 function(enable_doxygen DOXYGEN_THEME)
     # If not specified, use the top readme file as the first page
