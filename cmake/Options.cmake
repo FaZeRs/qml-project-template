7eda84b3f9fad7f2663946fee441ea7f5ee0f9df
Nauris Linde <naurislinde@gmail.com>
2024-05-15 17:32:44 +0300

    Added Hardening cmake configuration

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -1,3 +1,4 @@
+option(ENABLE_HARDENING "Enable hardening" ON)
 option(ENABLE_IPO "Enable IPO/LTO" OFF)
 option(ENABLE_COVERAGE "Enable coverage reporting" OFF)
 option(WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
